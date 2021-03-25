cd2eb38fd348325ed386f12bc5deb9b24cc3b58b
Shirley <berchel.shirley@gmail.com>
2021-03-25 16:52:00 +0100

    remove typing effect on index.html and add scrolling bar status on every page

--- script.js
+++ script.js
@@ -1,55 +1,4 @@
 console.log("Start of script");
-const textDisplay = document.getElementById('text');
-//console.log(textDisplay);
-const phrases = ['HEPHAESTOS'];
-//console.log(phrases);
-let i=0;
-let j=0;
-let currentPhrase = [];
-let isDeleting = false;
-let isEnd = false;
-
-function loop (){
-    isEnd=false;
-    textDisplay.innerHTML = currentPhrase.join('');
-    //console.log("Function loop");
-    //First element of the array
-    if (i<phrases.length){
-        //console.log(phrases[i]);
-        if (!isDeleting && j<phrases[i].length){
-            //console.log(phrases[i][j]);
-            currentPhrase.push(phrases[i][j]);
-            j++
-            textDisplay.innerHTML = currentPhrase.join('');
-        }
-
-        if(isDeleting && j<=phrases[i].length){
-            currentPhrase.pop(phrases[i][j]);
-            j--;
-            textDisplay.innerHTML = currentPhrase.join('');
-        }
-
-        if ( j==phrases[i].length){
-            idEnd=true;
-            isDeleting= true;
-        } 
-//If J=0, move on into the next element of the array
-        if(isDeleting && j == 0){
-            currentPhrase = [];
-//isDeleting is false cause we don't delete but adding letters again
-            isDeleting = false;
-            i++;
-//Looping again at end of the array
-            if( i == phrases.length){
-                i = 0;
-            }
-        }
-    }
-    setTimeout(loop,500);
-}
-loop();
-
-console.log("Fonctionnalités 2");
 
 document.addEventListener("DOMContentLoaded",function(){
     const progressbarInner = document.querySelector(".progress-bar-inner");
@@ -70,3 +19,5 @@ document.addEventListener("DOMContentLoaded",function(){
         progressbarInner.innerText = roundedPercent + "%";
     })
 })
+
+console.log("End of script");
