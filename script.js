c18fab98619d1bd7ab736c527df6419161207779
Shirley <berchel.shirley@gmail.com>
2021-03-25 11:21:57 +0100

    improving js typing effect to erase letters before display it again

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-console.log("Début du script");
-var textDisplay = document.getElementById("text");
-console.log(textDisplay);
-var phrases = ["HEPHAESTOS"];
-console.log(phrases);
-var i = 0 ;
-var j = 0 ;
+console.log("Start of script");
+const textDisplay = document.getElementById('text');
+//console.log(textDisplay);
+const phrases = ['HEPHAESTOS'];
+//console.log(phrases);
+let i=0;
+let j=0;
 let currentPhrase = [];
 let isDeleting = false;
-window.addEventListener('onload',decoup);
+let isEnd = false;
 
-function decoup(){
+function loop (){
+    isEnd=false;
     textDisplay.innerHTML = currentPhrase.join('');
-    //outer loop(tab)
-    if( i <= phrases.length ){
-    currentPhrase.push(phrases[i][j]);
-    //inner loop (tab el)
-    } if( !isDeleting && j <phrases[i].length){
-        j++;
-        //console.log('add a letter' + j);
-        textDisplay.innerHTML = currentPhrase.join('');
-    }
+    //console.log("Function loop");
+    //First element of the array
+    if (i<phrases.length){
+        //console.log(phrases[i]);
+        if (!isDeleting && j<phrases[i].length){
+            //console.log(phrases[i][j]);
+            currentPhrase.push(phrases[i][j]);
+            j++
+            textDisplay.innerHTML = currentPhrase.join('');
+        }
 
-    if(isDeleting && j<= phrases[i].length){
-        currentPhrase.pop(phrases[i][j]);
-        j--;
-        //console.log('rmv a letter' + j);
-        //console.log(currentPhrase)
+        if(isDeleting && j<=phrases[i].length){
+            currentPhrase.pop(phrases[i][j]);
+            j--;
+            textDisplay.innerHTML = currentPhrase.join('');
+        }
 
-        textDisplay.innerHTML = currentPhrase.join('');
-    }
-
-    if(j == phrases[i].length){
-        isDeleting = true;
-    }
-
-    if(isDeleting && j == 0){
-        currentPhrase = [];
-        isDeleting = false;
-        i++;
-        //Loop to repeat over and over the typing
-        
-            if(i == phrases.length){
+        if ( j==phrases[i].length){
+            idEnd=true;
+            isDeleting= true;
+        } 
+//If J=0, move on into the next element of the array
+        if(isDeleting && j == 0){
+            currentPhrase = [];
+//isDeleting is false cause we don't delete but adding letters again
+            isDeleting = false;
+            i++;
+//Looping again at end of the array
+            if( i == phrases.length){
                 i = 0;
             }
-    } 
-    setTimeout(decoup,500);
+        }
+    }
+    setTimeout(loop,500);
 }
-decoup();
+loop();
