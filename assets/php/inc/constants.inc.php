ecbe395761bc9dfdd91f49df43d02d9be779ac18
Shirley Berchel <berchel.shirley@gmail.com>
2021-06-16 17:07:19 +0200

    commit before new feature BE

---
+++ assets/php/inc/constants.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+/*Selon la structure d'accueil de l'appli,
+on adapte les constantes de connexion à la BDD*/
+
+switch ($_SERVER['HTTP_HOST']) {
+    case 'localhost':
+        define('HOST', 'localhost');
+        define('PORT', 3306);
+        define('DATA', 'greta60');
+        define('USER', 'root');
+        define('PASS', '');
+        break;
+    case 'baobab-svr5': // Fictif
+        define('HOST', 'baobab-svr5');
+        define('DATA', 'baobab-sql5');
+        define('USER', 'baobab-usr1');
+        define('PASS', 'aR5*hgt+7uIopp$');
+        break;
+    default:
+        // do nothing
+}
+
+//echo HOST;
+
+/*
+LOCAL
+host:localhost
+dname:greta60
+user: root
+pass:root
+
+REMOTE
+host:baobab-svr5
+dname: baobab-sql5
+user: baobab_usr1
+pass:aR5*hgt+7uIopp$
+*/
+
+?>
