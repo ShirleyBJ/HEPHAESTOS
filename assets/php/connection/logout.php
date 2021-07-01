188a818b5f90bd8d7ea58f9e8419a0d9aa97187d
Shirley Berchel <berchel.shirley@gmail.com>
2021-07-01 14:01:59 +0200

    add modif on lougout file

--- assets/php/connection/logout.php
+++ assets/php/connection/logout.php
@@ -1,9 +1,5 @@
 <?php
-        if(session_start()){
-            echo 'Session démarré';
-        }
     unset($_SESSION);
-    // unset($_SESSION['prenom']);
     if(session_destroy()){
         header('location: ../../../page5.php');
     }else{
