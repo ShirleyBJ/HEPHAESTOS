912181ab4e365b0dc8e5f6e2424fb90cecbe9c57
Shirley Berchel <berchel.shirley@gmail.com>
2021-07-01 14:01:06 +0200

    modif on logout file

--- assets/php/connection/logout.php
+++ assets/php/connection/logout.php
@@ -1,9 +1,9 @@
 <?php
-    //Déconnecter un utilisateur déja authentifié
-    session_start();
-    //session_destroy sert à détruire la session
-    // session_destroy();
-    unset($_SESSION['prenom']);
+        if(session_start()){
+            echo 'Session démarré';
+        }
+    unset($_SESSION);
+    // unset($_SESSION['prenom']);
     if(session_destroy()){
         header('location: ../../../page5.php');
     }else{
